8e13987d88f5ff9e26e5fd9f94060c8f51ebbb39
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-06-17 12:48:17 +0530

    tets changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';  // Import React (needed for JSX)
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SecondPage from "./components/SecondPage"; // Import BrowserRouter for routing
+
+function MainPage() {
+  return <div>Dashboard
+    <button onClick={() => {
+      window.location.href = '/dashboard/test-application/about';
+    }}>Go to About</button>
+  </div>;
+}
 
 function App() {
-  return (<div>Hello World</div>);
+  return (
+    <BrowserRouter><div>Hello World</div>
+      <Routes>
+        <Route path="" element={<MainPage />} />
+        <Route path="/dashboard/test-application/about" element={<SecondPage />} />
+      </Routes>
+
+
+    </BrowserRouter >);
 };
 
 export default App;
